03a288722335699fbc77a41955cd557672eaf0d1
Piotr Ostrowski <piotr.ostrowski@aol.com>
2021-03-08 10:49:48 +0100

    fix translate, add google key

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
 import pickle
+import cv2
 
-from googletrans import Translator
+from google.cloud import translate_v2
 from PIL import Image
 from cv2 import resize
 from pdf2image import convert_from_path
@@ -15,29 +16,17 @@ app = Flask(__name__, static_folder='static')
 path = '../ocr-party/models'
 
 
-
-def four_points_to_bbox(four_points):
-    pass  # TODO 
-
-
-def box_to_bbox(box: list) -> list:
-    """
-    bbox is (left_x, top_y, right_x, bottom_y),
-    whereas box is (left, top, width, height)
-    """
-    left_x, top_y = box[0], box[1]
-    right_x = box[0] + box[2]
-    bottom_y = box[1] + box[3]
-    return [left_x, right_x, top_y, bottom_y]
-
-
-def translate(img, points, text, translator: Translator):
-    bbox = points_to_bbox(points)
-    x1, x2, y1, y2 = box_to_bbox(box)
+def replace_and_translate(img, points, text, translator):
+    [x1, y1], [x2, y1], [x2, y2], [x1, y2] = points
+    x1 = int(x1)
+    x2 = int(x2)
+    y1 = int(y1)
+    y2 = int(y2)
+    print(x1, y1, x2, y2, img.shape)
     cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), -1)
-    translated = translator.translate(text, dest='en')
-    cv2.putText(img, translated.text, org=(x1, y1), font=cv2.FONT_HERSHEY_SIMPLEX,
-                color=(0, 0, 0)) # not sure if font size
+    translated = translator.translate(text)['translatedText']
+    cv2.putText(img, translated, org=(x1, y2), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                color=(0, 0, 0), fontScale=1) # not sure if font size
     return img 
 
 
@@ -73,13 +62,15 @@ def preview(filename):
 def translate(filename):
     with open('res.pkl', 'rb') as f:
         res = pickle.load(f)
-    image = convert_from_path(filename)[0]
-    image = np.array(image)
+    img = convert_from_path(filename)[0]
+    img = np.array(img)
     # res = reader.readtext(image)
-    for bbox, confidence,   
+    translator = translate_v2.Client()
+    for points, text, confidence in res:
+        img = replace_and_translate(img, points, text, translator)
     name = filename.split('.')[0]
     filename = name + '_translated.pdf' 
-    as_pil = Image.fromarray(image)
+    as_pil = Image.fromarray(img)
     as_pil.save('static/' + filename)
     return render_template('translate.html', filename=filename)
         
@@ -90,6 +81,7 @@ def download(filename):
 
 
 if __name__ == '__main__':
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './key.json'
     # reader = Reader(['pl'], model_storage_directory=path, gpu=True)
     app.run(host='0.0.0.0', debug=True)
     # TODO on leave page delete all static
